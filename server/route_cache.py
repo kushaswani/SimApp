c266a6d61a7e7c956d1924be11e9779de6910c99
Fredric Moezinia <moezinia@mit.edu>
2016-10-26 18:00:37 -0400

    from chew

---
+++ server/route_cache.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient, GEO2D
+
+class RouteCache:
+	
+	def __init__(self, dbname):
+		self.client = MongoClient()
+		self.db = self.client[dbname]
+		self.places = self.db.places
+
+	def hasRoute(self, ori, dst):
+		return (not (self.places.find_one({"ends": [ori,dst]}) is None))
+
+	def getRoute(self, ori, dst):
+		found = self.places.find_one({"ends": [ori, dst]})
+		return found["route"]
+
+	def setRoute(self, ori, dst, rte):
+		self.places.insert({"ends":[ori, dst], "route": rte})
+
+	def save(self):
+		pass
+
+	def close(self):
